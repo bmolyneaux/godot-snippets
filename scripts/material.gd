0e16267d869e8739bbc613a69a73e87f8e330338
Ben <bmolynea@gmail.com>
2020-02-08 18:53:12 -0800

    Update material instance description

--- scripts/material.gd
+++ scripts/material.gd
@@ -6,8 +6,10 @@ func _ready():
 	material.set_shader_param("color", Color.blue)
 
 func create_material_instance():
-	# This was necessary to enable a dynamic material instance
-	# in the ColorSprite tool class.
+	# Sharing a material also means sharing the instance.
+	# You either need to mark the material resource as unique or use duplicate
+	# the material as we do here.
+	# https://godotengine.org/qa/2866/how-do-i-make-material-shader-instances-2d
 	if not is_material_instance:
 		if material:
 			material = material.duplicate()
